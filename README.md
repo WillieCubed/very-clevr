fca7cfee9401b33b5d03bf0c9c1b4f9c63d11f64
Willie Chalmers III <willie@williecubed.me>
2022-06-24 15:15:30 -0500

    Set up project CLI and docs
    vc is the command line tool to train and evaluate the Very CLEVR system.
    The project is now installable using setup tools and pip.

--- README.md
+++ README.md
@@ -11,3 +11,22 @@ license: mit
 ---
 
 Check out the configuration reference at https://huggingface.co/docs/hub/spaces#reference
+
+These are files used to produce models that can be uploaded to Hugging Face.
+
+# Getting Started
+
+1. Clone the repository
+2. Install the CLI
+
+`pip install -e .`
+
+This will expose the `vc` CLI tool.
+
+## Demoing
+
+Simply run `vc interact` in a terminal.
+
+## Training
+
+Run `vc train` in a terminal. There are a few command line options.
--- setup.py
+++ setup.py
@@ -1,10 +1,23 @@
 from setuptools import setup
 
+with open("README.md") as f:
+    long_description = f.read()
+
 setup(
     name="very-clevr",
     version="0.1.0",
-    packages=["very_clevr"],
-    # ...
+    packages=["very_clevr", "much_clevr"],
+    description="",
+    long_description=long_description,
+    author="Willie Chalmers III",
+    author_email="willie@williecubed.me",
+    url="https://williecubed.github.io/very-clevr",
+    classifiers=[
+        "Development Status :: 1 - Planning",
+        "License :: OSI Approved :: MIT License",
+        "Natural Language :: English",
+        "Operating System :: OS Independent",
+    ],
     entry_points={
         "console_scripts": [
             "vc=very_clevr.utils:run_cli",
